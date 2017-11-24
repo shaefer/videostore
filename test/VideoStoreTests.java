ff95cb8af8aa5ec7a56c26ed1be1b7e29255cf1f
Daniel Shaefer <daniel.shaefer@hudl.com>
2017-11-24 09:35:37 -0600

    Rename test class to plural

--- test/VideoStoreTests.java
+++ test/VideoStoreTests.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class VideoStoreTest
+public class VideoStoreTests
 {
 	@BeforeEach
 	protected void setUp ()  {
